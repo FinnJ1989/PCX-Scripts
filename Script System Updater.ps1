e38d5c0f462c68d1b571a5952399c7900bc4fdd4
James Finnegan <82524023+FinnJ1989@users.noreply.github.com>
2026-05-01 09:14:38 -0400

    Update Script System Updater.ps1

--- Script System Updater.ps1
+++ Script System Updater.ps1
@@ -4,7 +4,7 @@ $RepoName = "PCX-Scripts"
 $Branch = "main"
 
 $BaseUrl = "https://raw.githubusercontent.com/$RepoOwner/$RepoName/$Branch" 
-$LocalRoot = "$env:LOCALAPPDATA\MyTool"
+$LocalRoot = "$env:LOCALAPPDATA\Faze"
 $Manifest = "$LocalRoot\manifest.json"
 $LocalVer = "$LocalRoot\local_versions.json"
 $LogFile = "$LocalRoot\update.log"
